e3d8b7cb8da4c805acd65d5abb038a78faf9c1bf
Adrien Tchounkeu <adrientchounkeu10@gmail.com>
2022-05-20 02:05:10 +0100

    remove unused codes

--- backend/app/crud/crud_user.py
+++ backend/app/crud/crud_user.py
@@ -1,8 +1,5 @@
 from typing import (
-    Any,
-    Dict,
     Optional,
-    Union,
 )
 from sqlalchemy.orm import Session
 from fastapi import UploadFile
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-from starlette.middleware.cors import CORSMiddleware
 
 from app.api.api_v1.api import api_router
 from app.core.config import settings
